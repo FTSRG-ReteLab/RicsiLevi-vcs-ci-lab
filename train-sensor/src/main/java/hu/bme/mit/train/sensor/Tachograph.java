0fef96c357dabfc0899944d5caea1dd27bb32507
Rorck <sz.ricsi@hotmail.com>
2018-02-19 08:07:54 -0800

    Tacho

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/Tachograph.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/Tachograph.java
@@ -1,11 +1,12 @@
 package hu.bme.mit.train.sensor;
 import com.google.common.collect.Table;
+import com.google.common.collect.HashBasedTable;
 
 public class Tachograph {
 	private Table<Integer,Integer,Integer> tachograph;
 	
 	public Tachograph(){
-		tachograph = Table.create();
+		tachograph = HashBasedTable.create();
 	}
 	
 	public void put(int time,int pos, int speed){
